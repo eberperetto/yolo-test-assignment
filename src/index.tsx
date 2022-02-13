5169aa76e0a308f190898c18754cd5fbc0ab2ab8
Éber Peretto <eberperetto@gmail.com>
2022-02-13 07:54:38 -0300

    Favicon and icons

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import "./index.css";
 import App from "./App";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
+/* Creates the Apollo Client instance
+Caching is enabled to speed up app, but its use should be reassessed because 
+users would always want the latest prices */
 const client = new ApolloClient({
   uri: "https://api.blocktap.io/graphql",
   cache: new InMemoryCache(),
